20e0f7fd946dbd7a1a6ad1b70c75267f06147ae1
Edison Wang <edisonwang03@gmail.com>
2022-11-20 15:42:40 -0500

    Completed UI, play again message, and docs

--- Driver.py
+++ Driver.py
@@ -1,128 +1,132 @@
 from TicTacToe import Board
-from time import sleep
 import random
 import pygame
+from pygame.locals import *
 
-
+# Initialize pygame
 pygame.init()
 
-screen_width = 300
-screen_height = 300
 
-screen = pygame.display.set_mode((screen_width,screen_height))
+# Initialize constants
+SCREEN_WIDTH = 300
+SCREEN_HEIGHT = 300
+LINE_WIDTH = 6
+GREEN = (0,255,0)
+RED = (255,0,0)
+BLUE = (0,0,255)
+YELLOW = (255,255,200)
+TEXT_FONT = pygame.font.SysFont(None, 40)
+
+
+# Initialize play again button
+playAgainRectangle = Rect(SCREEN_WIDTH // 2 - 80, SCREEN_HEIGHT // 2, 160, 50)
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption('TicTacToe')
 
-line_width = 6
 
-green = (0,255,0)
-red = (255,0,0)
-
-def displayGrid():
-    bg = (255,255,200)
+# Draw the grid divisions
+def drawGrid():
     grid = (50,50,50)
-    screen.fill(bg)
+    screen.fill(YELLOW)
     for x in range(1,3):
-        pygame.draw.line(screen,grid,(0,x*100),(screen_width,x*100),line_width)
-        pygame.draw.line(screen,grid,(x*100,0),(x*100,screen_height),line_width)
+        pygame.draw.line(screen,grid,(0,x*100),(SCREEN_WIDTH,x*100),LINE_WIDTH)
+        pygame.draw.line(screen,grid,(x*100,0),(x*100,SCREEN_HEIGHT),LINE_WIDTH)
         
 
+# Draw the user and computer markers
 def drawMarkers():
     x_pos = 0
     for x in board.grid:
         y_pos = 0
         for y in x:
             if y == 1:
-                pygame.draw.line(screen,green,(x_pos*100+15,y_pos*100+15),(x_pos*100+85,y_pos*100+85),line_width)
-                pygame.draw.line(screen,green,(x_pos*100+15,y_pos*100+85),(x_pos*100+85,y_pos*100+15),line_width)
+                pygame.draw.line(screen,GREEN,(x_pos*100+15,y_pos*100+15),(x_pos*100+85,y_pos*100+85),LINE_WIDTH)
+                pygame.draw.line(screen,GREEN,(x_pos*100+15,y_pos*100+85),(x_pos*100+85,y_pos*100+15),LINE_WIDTH)
             if y == -1:
-                pygame.draw.circle(screen,red,(x_pos*100+50,y_pos*100+50),38,line_width)
+                pygame.draw.circle(screen,RED,(x_pos*100+50,y_pos*100+50),38,LINE_WIDTH)
             y_pos +=1
         x_pos +=1
+        
+
+# Draw the game over UI     
+def drawResults(winner):
+    if winner == 1:
+        resultsText = "You won!"
+    elif winner == -1:
+        resultsText = "You lose..."
+    else:
+        resultsText = "It's a draw"
+    resultsImage = TEXT_FONT.render(resultsText,True,BLUE)
+    resultsRectangle = pygame.draw.rect(screen,GREEN,(SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 - 60,200,50))
+    screen.blit(resultsImage,resultsImage.get_rect(center = resultsRectangle.center))
+    
+    playAgainText = "Play again?"
+    playAgainImage = TEXT_FONT.render(playAgainText,True,BLUE)
+    pygame.draw.rect(screen,GREEN,playAgainRectangle)
+    screen.blit(playAgainImage,playAgainImage.get_rect(center = playAgainRectangle.center))
+     
             
 # Initialize game variables
-players = [1,-1]
-# currentPlayerTurn = random.choice(players)
+players = [1,-1] # 1 represents the user's symbol, X, while -1 represents the computer's symbol, O
+currentPlayerTurn = random.choice(players)
 board = Board(1,-1)
-currentPlayerTurn = -1
-    
-    
-
-
-playing = True
+gameOver = False
 done = False
+
+# Game loop
 while not done:
     
     # Add event handlers
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        if event.type == pygame.MOUSEBUTTONDOWN and currentPlayerTurn == 1 and playing:
+            
+        # User move
+        if event.type == pygame.MOUSEBUTTONDOWN and currentPlayerTurn == 1 and not gameOver:
             pos = pygame.mouse.get_pos()
             cell_x = pos[0]//100
             cell_y = pos[1]//100
             if board.grid[cell_x][cell_y] == 0:
                 board.grid[cell_x][cell_y] = 1
                 currentPlayerTurn *= -1
+        
+        # Reset game variables
+        if event.type == pygame.MOUSEBUTTONDOWN and gameOver:
+            pos = pygame.mouse.get_pos()
+            if playAgainRectangle.collidepoint(pos):
+                currentPlayerTurn = random.choice(players)
+                board = Board(1,-1)
+                gameOver = False           
     
-    if board.isUserWin():
-        print("Win")
-        playing = False
-    if board.isComputerWin():
-        print("Loss")
-        playing = False
-    if board.isDraw():
-        print("Draw")
-        playing = False
-              
-    if currentPlayerTurn == -1 and playing:
+    
+    # Check if the game is over
+    if board.isUserWin() or board.isComputerWin() or board.isDraw():
+        gameOver = True
+        
+         
+    # Computer move     
+    if currentPlayerTurn == -1 and not gameOver:
         board.computerMove()
         currentPlayerTurn*=-1
     
-    displayGrid()
+    
+    # Draw the game
+    drawGrid()
     drawMarkers()
     
+    # Draw the game over UI
+    if gameOver:
+        if board.isUserWin():
+            drawResults(1)
+        elif board.isComputerWin():
+            drawResults(-1)
+        else:
+            drawResults(0)
+    
+    
+    # Update the screen      
     pygame.display.update()
 
+
+# Close pygame
 pygame.quit()
-    
-"""
-
-user = input("Choose one of the two marks (X or O): ")
-if user == "X":
-    computer = "O"
-elif user == "O":
-    computer = "X"
-x = random.randint(0,1)
-if x == 0:
-    y = 1
-elif x == 1:
-    y = 0
-order = {user:x, 
-            computer:y}
-board = Board(user,computer)
-turn = 1
-
-
-while turn<=9:
-    print("Turn #" + str(turn))
-    board.display()
-    if turn%2==order[user]:
-        position = int(input("It's your turn! Choose a number between 1-9 to place your mark: "))
-        board.userMove(position)
-        if(board.isPlayerWin()):
-            board.display()
-            print("You won")
-            break
-    elif turn%2==order[computer]:
-        print("The computer is making a move...")
-        sleep(1)
-        board.computerMove()
-        if(board.isComputerWin()):
-            board.display()
-            print("You lose")
-            break
-    turn+=1
-    if board.isDraw():
-        board.display()
-        print("It's a tie")
-"""
--- TicTacToe.py
+++ TicTacToe.py
@@ -1,57 +1,92 @@
 class Board():
-    """A class to simulate a Tic-Tac-Toe board"""
+    """ A class to represent a Tic-Tac-Toe game
+    """
     
     def __init__(self,user,computer):
+        """ Constructs the grid and rules
 
+        Args:
+            user (int): unique number representing the user's mark
+            computer (int): unique number representing the computer's mark
+        """
         self.grid = [[0,0,0],[0,0,0],[0,0,0]]
         self.user = user
         self.computer = computer
         self.winningPositions = {((0,0), (0,1), (0,2)), ((1,0), (1,1), (1,2)), ((2,0), (2,1), (2,2)), 
                         ((0,0), (1,0), (2,0)), ((0,1), (1,1), (2,1)), ((0,2), (1,2), (2,2)),
                         ((0,0), (1,1), (2,2)), ((0,2), (1,1), (2,0))}
-       
-       
-    def display(self):
-        print(" " + str(self.grid[0][0]) + " | " + str(self.grid[0][1]) + " | " + str(self.grid[0][2]))
-        print("-----------")
-        print(" " + str(self.grid[1][0]) + " | " + str(self.grid[1][1]) + " | " + str(self.grid[1][2]))
-        print("-----------")
-        print(" " + str(self.grid[2][0]) + " | " + str(self.grid[2][1]) + " | " + str(self.grid[2][2]))
         
       
     def markGrid(self,i,j,mark):
+        """ Marks the grid at a given tile with a given mark
+
+        Args:
+            i (int): row index of the tile
+            j (int): column index of the tile
+            mark (int): unique number representing the player's mark
+        """
         self.grid[i][j] = mark
 
 
     def unmarkGrid(self,i,j):
+        """ Unmarks the grid at a given tile
+
+        Args:
+            i (int): row index of the tile
+            j (int): column index of the tile
+        """
         self.grid[i][j] = 0
          
     
     def isUserWin(self):
+        """ Determines whether the user has won yet or not
+
+        Returns:
+            bool: whether the user has won yet or not
+        """
         for((x1,x2),(y1,y2),(z1,z2)) in self.winningPositions:
             if self.grid[x1][x2] == self.user and self.grid[y1][y2] == self.user and self.grid[z1][z2] == self.user:
                 return True
+            
         return False
     
     
     def isComputerWin(self):
+        """ Determines whether the computer has won yet or not
+
+        Returns:
+            bool: whether the user has won yet or not
+        """
         for((x1,x2),(y1,y2),(z1,z2)) in self.winningPositions:
             if self.grid[x1][x2] == self.computer and self.grid[y1][y2] == self.computer and self.grid[z1][z2] == self.computer:
                 return True
+            
         return False
     
   
     def isDraw(self):
+        """ Determines whether the game has a draw
+
+        Returns:
+            bool: whether there is a draw or not
+        """
         if self.isComputerWin() or self.isUserWin():
             return False
+        
         for row in self.grid:
             for tile in row:
                 if tile==0:
                     return False
+                
         return True
         
         
     def possibleMoves(self):
+        """ Determines the possible moves that can be made on the current grid
+
+        Returns:
+            list : list of tuples, where each tuple are the indexes of tiles
+        """
         moves = set()
         for i in range(len(self.grid)):
             for j in range(len(self.grid[i])):
@@ -61,6 +96,14 @@ class Board():
                     
                     
     def minimax(self,isMaximizing):
+        """ Uses minimax algorithm to calculate the outcome score for each move and the best outcome score
+
+        Args:
+            isMaximizing (bool): whether calculating the maximum or minimum score
+
+        Returns:
+            int: the best score
+        """
         if self.isComputerWin():
             return 1
         elif self.isUserWin():
@@ -70,7 +113,6 @@ class Board():
         
         if isMaximizing:
             bestScore = float("-inf")
-            
             for (i,j) in self.possibleMoves():
                     self.markGrid(i,j,self.computer)
                     score = self.minimax(False)
@@ -78,20 +120,20 @@ class Board():
                     if score>bestScore:
                         bestScore = score
             return bestScore
-        
         else:
             bestScore = float("inf")
-            
             for (i,j) in self.possibleMoves():
                 self.markGrid(i,j,self.user)
                 score = self.minimax(True)
                 self.unmarkGrid(i,j)
                 if score<bestScore:
                     bestScore = score
-            return bestScore
-         
+            return bestScore   
+     
             
     def computerMove(self):
+        """ Makes the best move for the computer and marks the tile
+        """
         bestScore = float("-inf")
         moves = self.possibleMoves()
         
@@ -108,6 +150,12 @@ class Board():
         
         
     def userMove(self,i,j):
+        """ Makes the move that the user wants to make and marks the tile
+
+        Args:
+            i (int): the row index of the tile
+            j (int): the column index of the tile
+        """
         self.markGrid(i,j,self.user)
         
                 
